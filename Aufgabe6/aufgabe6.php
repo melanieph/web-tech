b78b8f974ac0bac7ca54662d4044de309e64bcfd
Melanie Hoffmann <s0563911@htw-berlin.de>
2018-12-20 10:29:23 +0100

    aufgabe06

--- Aufgabe6/aufgabe6.php
+++ Aufgabe6/aufgabe6.php
@@ -9,51 +9,42 @@
 <body class="container-fluid">
 <h1>Datei einlesen</h1>
 <?php
-
-
-
-/*
- * falls das Zeilenende der einzulesenden Datei nicht korrekt
- * erkannt werden sollte, dann sollte die folgende Anweisung
- * ausgeführt werden
- */
 ini_set("auto_detect_line_endings", true);
-/* Öffnen der Datei zum Lesen "r"
- * Datei muss im gleichen Verzeichnis liegen wie aufgabe6.php
- * andernfalls Pfadangabe ändern
- * @ unterdrückt evtl Warnungen
- */
+
 $file = @fopen ( "./mockdatatext", "r" );
-/*
- * wenn das Öffnen der Datei nicht funktioniert hat, ist $file FALSE
- * ansonsten steht der Dateizeiger am Anfang der ersten Zeile
- */
+
 if (! $file) {
     echo "Es ist ein Problem beim Öffnen der Datei 'mockupdatatext' aufgetreten.";
 } else {
-    /*
-     * feof - end of file
-     * prüft, ob ein Dateizeiger am Ende der Datei steht
-     */
+    
+    echo "<div class='row'>";
+    $counter=0;
     while ( ! feof ( $file ) ) {
+      
+        if ($counter%10==0){
 
-        echo fgets($file) . "<br />";  /** read file line by line */
-        /*
-         * fegts() liest eine Zeile einer Datei aus
-         * fgets() gibt einen String zürück
-         * nach Aufruf von fgets() steht der Dateizeiger
-         * in der nächsten Zeile (außer, es wurde eine
-         * Leselänge als 2. Parameter fgets übergeben)
-         */
-        $current_line = fgets ( $file );
-        echo $current_line . "<br/>";
-        /*
-         * Sie können für die Lösung der Aufgabe davon ausgehen,
-         * dass jeder Datensatz aus 4 Zeilen besteht und dass zwischen
-         * den einzelnen Datensätzen immer genau eine Leerzeile ist.
-         */
+            echo "<div class='col-xl-6 col-lg-6 col-md-12' style='background-color:dimgray; margin-top:10px; margin-bottom:10px;'>";  
+            echo "<ul class='list-group' style='padding:10px;'>";
+        }
+
+        
+        $vorname=fgets($file);
+        $nachname=fgets($file);
+        $email=fgets($file);
+        $ipAdresse=fgets($file);
+        $leereZeile=fgets($file);
+
+        echo "<li class='list-group-item'>".$vorname." ".$nachname." (<a href='mailto:".$email."'>". $email."</a>) </li>";
+
+        if($counter%10==9){
+            echo "</ul>";
+            echo "</div>";
+        }
+
+        $counter++;
     }
-
+    echo "</ul>";
+    echo "</div>";
     fclose ( $file );
 }
 ?>
